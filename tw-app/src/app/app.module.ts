6ad349dad8453109930af0ec13813ef19e97fc28
henry300 <henry.teigar@gmail.com>
2017-04-22 00:03:41 +0300

    Add client and camera dummy comonents

--- tw-app/src/app/app.module.ts
+++ tw-app/src/app/app.module.ts
@@ -2,17 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CameraComponent } from './components/camera/camera.component';
+import { ClientComponent } from './components/client/client.component';
+
+const appRoutes: Routes = [
+  { path: '', component: ClientComponent },
+  { path: 'camera', component: CameraComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CameraComponent,
+    ClientComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
