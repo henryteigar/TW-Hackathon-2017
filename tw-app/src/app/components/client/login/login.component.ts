fde9c3a5ef7b53816fcd59bbe311bc444eb7500b
henry300 <henry.teigar@gmail.com>
2017-04-22 14:32:31 +0300

    Finish welcome screen

--- tw-app/src/app/components/client/login/login.component.ts
+++ tw-app/src/app/components/client/login/login.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  showWelcome = false
+  showLogin = !this.showWelcome;
+
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    this.showWelcome = !this.showWelcome;
+    this.showLogin = !this.showWelcome;
   }
 
 }
